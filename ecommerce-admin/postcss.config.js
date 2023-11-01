d9b0d1275df710355fb26fa1eff4b68a8ea093fb
Shell-thon <shellterminal0@gmail.com>
2023-11-01 18:16:12 +0100

    postcss config setup i.e tailwind css

--- ecommerce-admin/postcss.config.js
+++ ecommerce-admin/postcss.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 }
