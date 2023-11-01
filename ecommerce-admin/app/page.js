4d542b26cbd99be3bbf553e9a9ef54edf97465b9
Shell-thon <shellterminal0@gmail.com>
2023-11-01 19:22:30 +0100

    fix  config issue with tailwind add auth for login

--- ecommerce-admin/app/page.js
+++ ecommerce-admin/app/page.js
@@ -1,7 +1,9 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    
+    <div className={"bg-blue-900 w-screen h-screen flex items-center"}>
+      <div className="text-center w-full">
+        <button className="bg-white p-2 px-4 rounded-lg" style={{color: "black"}}>Login with Google</button>
+      </div>
+    </div>
   )
 }
