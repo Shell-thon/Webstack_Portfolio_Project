5d31c53115e58b4f1a9bd27607c98c0250f9b4bf
shell-thon <shellterminal0@gmail.com>
2023-11-02 22:51:14 +0100

    update views.py alert.html works well

--- ecomm/accounts/views.py
+++ ecomm/accounts/views.py
@@ -8,27 +8,46 @@ from django.http import HttpResponseRedirect
 
 
 def login_page(request):
+
+if request.method == 'POST':
+        email= request.POST.get('email')
+        password = request.POST.get('password')
+        user_obj = User.objects.filter(username= email)
+
+        if not user_obj.exists():
+            messages.warning(request, "Account not found")
+            return HttpResponseRedirect(request.path_info)
+
+        print(email)
+
+        user_obj = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=email,)
+        user_obj.set_password(password)
+        user_obj.save()
+
+        messages.success(request, "Check your inbox email sent to your mail.")
+        return HttpResponseRedirect(request.path_info)
     return render(request, "accounts/login.html")
 
 def register_page(request):
 
     if request.method == 'POST':
         first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
+        last_name = ''  # Set last_name to an empty string
         email= request.POST.get('email')
         password = request.POST.get('password')
-
         user_obj = User.objects.filter(username= email)
 
         if user_obj.exists():
             messages.warning(request, "Email already exists")
             return HttpResponseRedirect(request.path_info)
 
+        print(email)
+
         user_obj = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=email,)
         user_obj.set_password(password)
         user_obj.save()
 
-        messages.warning(request, "Check your inbox email sent to your mail.")
+        messages.success(request, "Check your inbox email sent to your mail.")
         return HttpResponseRedirect(request.path_info)
 
     
