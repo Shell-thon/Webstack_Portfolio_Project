eeb94f6d56ba03984ac4ad7de4606cc233f4b927
shell-thon <shellterminal0@gmail.com>
2023-11-02 05:37:19 +0100

    update

--- ecomm/base/models.py
+++ ecomm/base/models.py
@@ -1,10 +1,10 @@
+import uuid
 from django.db import models
-
 
 class BaseModel(models.Model):
     uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
--- ecomm/products/models.py
+++ ecomm/products/models.py
@@ -2,23 +2,22 @@ from django.db import models
 from base.models import BaseModel
 
 
-
-
 class Category(BaseModel):
     category_name = models.CharField(max_length=100,)
-    category_image = models.ImageField(upload="categories")
+    slug = slug = models.SlugField(unique=True, null=True, blank=True)
+    category_image = models.ImageField(upload_to="categories")
 
 
 
 class Product(BaseModel):
     product_name = models.CharField(max_length=100,)
+    slug = models.SlugField(unique=True, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     price = models.IntegerField()
     product_description = models.TextField()
-    ription = models.TextField()
 
 
 
 class ProductImage(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_images")
-    image = models.ImageField(upload="product")
+    image = models.ImageField(upload_to="product")
