e91536a29c91a610b498df92ed3505b0535f4652
shell-thon <shellterminal0@gmail.com>
2023-11-02 03:44:30 +0100

    update

--- ecommerce-admin/app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className={"bg-blue-900 w-screen h-screen flex items-center"}>
-      <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-lg" style={{color: "black"}}>Login with Google</button>
-      </div>
-    </div>
-  )
-}
