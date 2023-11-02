37dea6df576663d4bf4c99f52c6a80f58f036b72
shell-thon <shellterminal0@gmail.com>
2023-11-02 08:56:30 +0100

    Update

--- ecomm/accounts/views.py
+++ ecomm/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,3 +8,14 @@ from django.shortcuts import render
 
 def login_page(request):
     return render(request, "accounts/login.html")
+
+def register_page(request):
+
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email= request.POST.get('email')
+        password = request.POST.get('password')
+
+
+    return render(request, "accounts/register.html")
