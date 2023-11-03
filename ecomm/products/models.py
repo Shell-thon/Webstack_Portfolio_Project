90c6ac608a73318a2358a75a6814a301b19f3553
shell-thon <shellterminal0@gmail.com>
2023-11-03 17:07:34 +0100

    update

--- ecomm/products/models.py
+++ ecomm/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from base.models import BaseModel
+from django.utils.text import slugify
 
 
 class Category(BaseModel):
@@ -9,12 +10,18 @@ class Category(BaseModel):
 
 
 
+def save(self, *args, **kwargs):
+    self.slug = slugify(self.title)
+    super(Category, self).save(*args, **kwargs)
+
 class Product(BaseModel):
     product_name = models.CharField(max_length=100,)
     slug = models.SlugField(unique=True, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     price = models.IntegerField()
     product_description = models.TextField()
+
+
 
 
 
