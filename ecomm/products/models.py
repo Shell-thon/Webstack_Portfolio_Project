5b77184892be6bd6276ce86f2acb3fb0c1c3d476
shell-thon <shellterminal0@gmail.com>
2023-11-04 02:26:21 +0100

    added the models to getproducts and show in views

--- ecomm/products/models.py
+++ ecomm/products/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from base.models import BaseModel
 from django.utils.text import slugify
+from django.shortcuts import render
+from django.http import Http404
 
 class Category(BaseModel):
     category_name = models.CharField(max_length=100)
@@ -18,14 +20,14 @@ class ColorVariant(BaseModel):
     color_name = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.color_name
 
 class SizeVariant(BaseModel):
     size_name = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
     
-    def __str__(self):
+    def __str__(self) -> str:
         return self.size_name
 
 class Product(BaseModel):
@@ -44,6 +46,17 @@ class Product(BaseModel):
     def __str__(self):
         return self.product_name
 
+
+
+        def get_product_price_by_size(self, size):
+            return self.price + SizeVariant.objects.get(size_name=size).price
+
 class ProductImage(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_images")
     image = models.ImageField(upload_to="product")
+
+class Coupon(BaseModel):
+    coupon_code = models.CharField(max_length=10)
+    is_expired = models.BooleanField(default=False)
+    discount_price = models.IntegerField(default=100)
+    minimum_amount = models.IntegerField(default=500)
--- ecomm/products/views.py
+++ ecomm/products/views.py
@@ -1,17 +1,16 @@
 from pydoc import render_doc
+from tkinter import E
 from django.shortcuts import render
 from products.models import Product
-# Create your views here.
 
 
 
 
-
-
-def get_product(request, slug):
+def get_product(request , slug):
     try:
-        product = Product.objects.get(slug=slug)
-        return render(request, 'product/product.html', context = {'product': product})
+        product = Product.objects.get(slug =slug)
+        context = {'product': product}
+        return render(request  , 'product/product.html' , context = context)
 
     except Exception as e:
-        print (e)
+        print(e)
