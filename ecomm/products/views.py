4b7f338d36d274ad74107af9202511f79121d9af
shell-thon <shellterminal0@gmail.com>
2023-11-05 01:12:28 +0100

    updated the project erro

--- ecomm/products/views.py
+++ ecomm/products/views.py
@@ -1,16 +1,17 @@
-from pydoc import render_doc
-from tkinter import E
 from django.shortcuts import render
 from products.models import Product
 
 
 
-
-def get_product(request , slug):
+def get_product(request, slug):
     try:
-        product = Product.objects.get(slug =slug)
-        context = {'product': product}
-        return render(request  , 'product/product.html' , context = context)
+        product = Product.objects.get(slug=slug)
+
+        if request.GET.get('size'):
+            size = request.GET.get('size')
+            print(size)
+
+        return render(request, 'product/product.html', context = {'product' : product})
 
     except Exception as e:
         print(e)
