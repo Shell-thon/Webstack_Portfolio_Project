ca169666b798aeeed19759a8e13c1bca1e81e2d2
shell-thon <shellterminal0@gmail.com>
2023-11-05 02:35:53 +0100

    update on views

--- ecomm/products/views.py
+++ ecomm/products/views.py
@@ -6,10 +6,13 @@ from products.models import Product
 def get_product(request, slug):
     try:
         product = Product.objects.get(slug=slug)
-
+        context = {'product' : product}
         if request.GET.get('size'):
             size = request.GET.get('size')
-            print(size)
+            price = product.get_product_price_by_size(size)
+            context['selected_size'] = size
+            context['updated_price'] = price
+            print(price)
 
         return render(request, 'product/product.html', context = {'product' : product})
 
