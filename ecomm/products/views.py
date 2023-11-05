89c8b6efa3452ccf3ed45e591621acef87474511
shell-thon <shellterminal0@gmail.com>
2023-11-05 02:54:58 +0100

    cleanest version

--- ecomm/products/views.py
+++ ecomm/products/views.py
@@ -14,7 +14,7 @@ def get_product(request, slug):
             context['updated_price'] = price
             print(price)
 
-        return render(request, 'product/product.html', context = {'product' : product})
+        return render(request, 'product/product.html', context = context)
 
     except Exception as e:
         print(e)
